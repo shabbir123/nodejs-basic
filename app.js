5546ebf2b673af105bc7e4406999a01e22d5db30
shabbir123 <shabbir43.rizvi@gmail.com>
2024-11-08 20:18:15 +0530

    update file

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const text = fs.readFileSync('Files/input.txt','utf-8')
-console.log(text)
+//console.log(text)
 
 let content = `Data Read from input.txt:${text} \n created at ${new Date()}`
 fs.writeFileSync('Files/output.txt',content)
+
+
+fs.readFile('Files/input.txt','utf-8',function(err,data){
+    console.log(data)
+
+})
+console.log("reading file")
